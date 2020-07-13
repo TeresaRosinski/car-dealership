748d3e3fe31d50b3b1fe0bd0957c29bf038c24a7
Jeff Kim <kim2jy@hotmail.com>
2020-07-13 13:27:03 -0700

    Initial commit; up through lesson on Getters and Setters
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

---
+++ Models/Car.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace Dealership.Models
+{
+  public class Car
+  {
+    private string _makeModel;
+    private int _price;
+    private int _miles;
+
+    public Car(string makeModel, int price, int miles)
+    {
+      _makeModel = makeModel;
+      _price = price;
+      _miles = miles;
+    }
+
+    public string GetMakeModel()
+    {
+      return _makeModel;
+    }
+
+    public int GetPrice()
+    {
+      return _price;
+    }
+
+    public void SetPrice(int newPrice)
+    {
+      _price = newPrice;
+    }
+
+    public int GetMiles()
+    {
+      return _miles;
+    }
+
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (Price < maxPrice);
+    }
+  }
+}
